0198156ccf2f908555d075407415b0c412455cf5
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2025-06-08 14:42:32 +0900

    Fix: encode URLs in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,5 +4,16 @@ module.exports = {
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
-  priority: 0.7
+  priority: 0.7,
+  transform: async (config, path) => {
+    // 경로를 URL 인코딩
+    const encodedPath = encodeURI(path);
+    return {
+      loc: encodedPath,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
 } 
