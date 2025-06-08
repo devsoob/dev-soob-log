dc2f522db45908fc84689c6dde7ad324b920d11c
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2025-06-08 14:32:31 +0900

    fix: sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,11 +4,5 @@ module.exports = {
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
-  priority: 0.7,
-  exclude: ['/server-sitemap.xml'],
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://dev-log-pi.vercel.app/server-sitemap.xml',
-    ],
-  },
+  priority: 0.7
 } 
