410102b10cf4a6522396da6f6be45146b3b15bcc
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2025-06-15 18:48:18 +0900

    Design: PostCard

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { UnifiedPost } from '@/types/post';
 import Tag from './Tag';
 
@@ -25,15 +25,19 @@ function formatDate(date: string) {
 }
 
 function PostCard({ post }: PostCardProps) {
+  const router = useRouter();
   const formattedDate = React.useMemo(() => formatDate(post.date), [post.date]);
 
+  const handleClick = () => {
+    router.push(`/posts/${post.slug}`);
+  };
+
   return (
-    <div className="block border border-black dark:border-white rounded-lg p-6 mb-4">
-      <Link 
-        href={`/posts/${post.slug}`}
-        className="block"
-        prefetch={true}
-      >
+    <div 
+      className="block border border-black dark:border-white rounded-lg mb-4 cursor-pointer"
+      onClick={handleClick}
+    >
+      <div className="p-6">
         <article>
           <h2 className="text-2xl font-bold mb-2 text-black dark:text-white">{post.title}</h2>
           <div className="text-black dark:text-white text-sm mb-3">
@@ -41,14 +45,14 @@ function PostCard({ post }: PostCardProps) {
           </div>
           <p className="text-black dark:text-white mb-4">{post.description}</p>
         </article>
-      </Link>
-      {post.tags && post.tags.length > 0 && (
-        <div className="flex flex-wrap gap-2 mt-4">
-          {post.tags.map((tag) => (
-            <Tag key={tag} name={tag} />
-          ))}
-        </div>
-      )}
+        {post.tags && post.tags.length > 0 && (
+          <div className="flex flex-wrap gap-2 mt-4">
+            {post.tags.map((tag) => (
+              <Tag key={tag} name={tag} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
