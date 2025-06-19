ebc01a840b2cccdb11693223775eaa78d09f59ae
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2025-06-19 22:28:27 +0900

    Remove tag click functionality and tag page

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { UnifiedPost } from '@/types/post';
-import Tag from './Tag';
 import Link from 'next/link';
 
 interface PostCardProps {
